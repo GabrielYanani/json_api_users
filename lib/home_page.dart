d05cf885707c64d137301d1427d052e26cf63709
GabrielYanani <gabyyanani@gmail.com>
2021-12-20 21:18:20 -0300

    solucionad0

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:json_api_users/main.dart';
 import 'package:json_api_users/mis_contactos2.dart';
-// import 'package:json_api_users/usuarios.dart';
 
-// import 'mis_contactos2.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
