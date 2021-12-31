9e37e742403cae470e0ac0bf46cfbf9e6e54f5ce
GabrielYanani <gabyyanani@gmail.com>
2021-12-31 19:18:40 -0300

    provider problema en getUser

--- lib/controlador.dart
+++ lib/controlador.dart
@@ -1,19 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:json_api_users/usuarios.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<List<User>> getUsuarios() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? datosJson;
-  try {
-    Client http = Client();
-    Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
-    Response respuesta = await http.get(uri);
-    prefs.setString('user', respuesta.body);
-    datosJson = respuesta.body;
-  } catch (e) {
-    datosJson = prefs.getString('user');
+class UsuarioProvider extends ChangeNotifier {
+  final List<User> _usuarios = [];
+  List<User> get usuarios => _usuarios;
+
+  Future<List<User>> getUsuarios() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? datosJson;
+    try {
+      Client http = Client();
+      Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
+      Response respuesta = await http.get(uri);
+      prefs.setString('user', respuesta.body);
+      datosJson = respuesta.body;
+    } catch (e) {
+      datosJson = prefs.getString('user');
+    }
+    final List<User> user = datosFromJson(datosJson!);
+    
+    _usuarios.clear();
+    _usuarios.addAll(user);
+     notifyListeners();
   }
-  final List<User> user = datosFromJson(datosJson!);
-  return user;
 }
--- lib/paginas/home_page.dart
+++ lib/paginas/home_page.dart
@@ -1,31 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:json_api_users/controlador.dart';
-import 'package:json_api_users/widget/lista_de_contactos.dart';
+import 'package:json_api_users/paginas/detalle_de_contactos.dart';
+import 'package:json_api_users/usuarios.dart';
+import 'package:provider/provider.dart';
 
-class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AGENDA TELEFONICA'),
-        centerTitle: true,
+    return RefreshIndicator(
+      onRefresh: () => context.read<UsuarioProvider>().getUserData(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: SafeArea(
+          // Consumer de TIPO UsuarioProvider
+          child: Consumer<UsuarioProvider>(
+            builder: (BuildContext context, data, _) {
+              // if (data.estaCargandoDatos == true) {
+              //   return const Center(
+              //     child: CircularProgressIndicator(),
+              //   );
+              // }
+
+              if (data.usuarios.isEmpty) {
+                return const Center(
+                  child: Text('Lista vacia'),
+                );
+              }
+
+              return ListView(
+                children: crearListadoDeUsuarios(data.usuarios, context),
+              );
+            },
+          ),
+        ),
       ),
-      body: FutureBuilder(
-          future: getUsuarios(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return ListaDeContactos(snapshot.data);
-            }
-            return const Center(child: CircularProgressIndicator());
-          }),
     );
+  }
+
+  List<Widget> crearListadoDeUsuarios(List<User> usuarios, context) {
+    return usuarios.map((User usuario) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => DetalleDeContactos(usuario),
+                ));
+          },
+          tileColor: Colors.orange,
+          title: Text(usuario.name),
+        
+        ),
+      );
+    }).toList();
   }
 }
