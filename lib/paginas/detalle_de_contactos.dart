cfe7fa5125916746fb6f3bf461e7609702bc84aa
GabrielYanani <gabyyanani@gmail.com>
2021-12-31 17:58:31 -0300

    master2 con mostrar detalles+datos local+try/catch+estetica

--- lib/paginas/detalle_de_contactos.dart
+++ lib/paginas/detalle_de_contactos.dart
@@ -8,16 +8,42 @@ class DetalleDeContactos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Id: ${usuario.id}'),
-            Text('Nombre: ${usuario.name}'),
-            Text('Compania: ${usuario.company.name}'),
-            Text('Email: ${usuario.email}'),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Id: ${usuario.id}',
+                textScaleFactor: 1.5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                    color: Colors.grey,
+                    height: 180,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'Nombre: ${usuario.name}',
+                          textScaleFactor: 1.5,
+                        ),
+                        Text(
+                          'Compania: ${usuario.company.name}',
+                          textScaleFactor: 1.5,
+                        ),
+                        Text(
+                          'Email: ${usuario.email}',
+                          textScaleFactor: 1.5,
+                        ),
+                      ],
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
