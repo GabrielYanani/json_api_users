31912e86eff03adfedd4d5c7dbf29b410c74e787
GabrielYanani <gabyyanani@gmail.com>
2021-12-31 17:13:35 -0300

    master2 con mostrar detalles+datos local+try/catch

--- lib/controlador.dart
+++ lib/controlador.dart
@@ -4,10 +4,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<List<User>> getUsuarios() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  Client http = Client();
-  Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
-  Response respuesta = await http.get(uri);
-  prefs.setString('user', respuesta.body);
-  final List<User> user = datosFromJson(respuesta.body);
+  String? datosJson;
+  try {
+    Client http = Client();
+    Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
+    Response respuesta = await http.get(uri);
+    prefs.setString('user', respuesta.body);
+    datosJson = respuesta.body;
+  } catch (e) {
+    datosJson = prefs.getString('user');
+  }
+  final List<User> user = datosFromJson(datosJson!);
   return user;
 }
